916fae6060690f1f96897192b645a1f73eb8befe
salockhart <salexlockhart@gmail.com>
2017-04-05 14:38:43 -0300

    front end init

---
+++ docs/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+$(function () {
+    const phraseInput = $('#phrase');
+
+    phraseInput.keyup(function (event) {
+        const value = phraseInput.val();
+        if (value.length === 0) {
+            return;
+        }
+
+        const encoded = encodeURIComponent(value);
+
+        $.get("https://clap-as-a-service.herokuapp.com/clap?phrase=" + encoded)
+            .done(function (event) {
+                $('#clap').html(event);
+            });
+    });
+
+    $.urlParam = function (name) {
+        const results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        return results && results[1];
+    }
+
+    const urlPhrase = $.urlParam('phrase');
+
+    if (urlPhrase) {
+        phraseInput.val(urlPhrase);
+        phraseInput.keyup();
+    }
+});
