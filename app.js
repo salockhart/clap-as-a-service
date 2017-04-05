33dccf7bce0d37bae492d942958b40303ae3678c
salockhart <salexlockhart@gmail.com>
2017-04-05 18:45:25 -0300

    emoji support in docs

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('port', port);
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
--- docs/index.js
+++ docs/index.js
@@ -1,44 +1,153 @@
 'use strict';
 
-$(function () {
-    let typingTimer;
-    const doneTypingInterval = 500;
+let typingTimer;
+const doneTypingInterval = 500;
 
-    const phraseInput = $('#phrase');
+const emojis = [
+    '👏',
+    '💪',
+    '👌',
+    '👍',
+    '💩',
+    '🌸',
+    '🦆',
+    '🍝',
+    '🔫',
+    '🦀',
+    '💯',
+    '💰',
+    '🚽',
+    '🐴',
+    '🕳️',
+    '❤️',
+    '🆒',
+    '😂',
+    '💀',
+    '🍆',
+    '😎',
+    '🖕',
+    '🇨🇦',
+    '+'
+];
 
-    phraseInput.keyup(function () {
-        clearTimeout(typingTimer);
-        typingTimer = setTimeout(doneTyping, doneTypingInterval);
-    });
+let selectedEmoji = emojis[0];
 
-    phraseInput.keydown(function () {
-        clearTimeout(typingTimer);
-    });
+function onKeyUp() {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(submitPhrase, doneTypingInterval);
+}
 
-    //user is "finished typing," do something
-    function doneTyping() {
-        const value = phraseInput.val();
-        if (value.length === 0) {
-            return;
-        }
+function onKeyDown() {
+    clearTimeout(typingTimer);
+}
 
-        const encoded = encodeURIComponent(value);
+function submitPhrase() {
+    const value = $('#phrase').val();
+    let emoji = $('#custom-emoji').val();
 
-        $.get("https://clap-as-a-service.herokuapp.com/clap?phrase=" + encoded)
-            .done(function (event) {
-                $('#clap').html(event);
-            });
+    if (value.length === 0) {
+        return;
     }
 
-    $.urlParam = function (name) {
+    if (emoji.length === 0) {
+        console.log('No Custom Emoji');
+        emoji = selectedEmoji;
+    }
+
+    const encodedPhrase = encodeURIComponent(value);
+    const encodedEmoji = encodeURIComponent(emoji);
+
+    $.get('https://clap-as-a-service.herokuapp.com/clap?phrase=' + encodedPhrase + '&emoji=' + encodedEmoji)
+        .done(function (event) {
+            $('#clap').html(event);
+        });
+}
+
+function handleUrlParams() {
+    function urlParam(name) {
         const results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
         return results && results[1];
     }
 
-    const urlPhrase = $.urlParam('phrase');
+    const urlPhrase = urlParam('phrase');
+    let urlEmoji = urlParam('emoji');
+
+    if (urlEmoji) {
+        urlEmoji = decodeURIComponent(urlEmoji);
+        const idx = emojis.indexOf(urlEmoji);
+        if (idx !== -1) {
+            onEmojiSelect({
+                target: $('#sample-emoji-container button')[idx]
+            })
+        } else {
+            onOpenCustomEmoji();
+            $('#custom-emoji').val(urlEmoji);
+        }
+    }
 
     if (urlPhrase) {
-        phraseInput.val(decodeURIComponent(urlPhrase));
-        phraseInput.keyup();
+        $('#phrase').val(decodeURIComponent(urlPhrase));
     }
+
+    submitPhrase();
+}
+
+function fillEmojiSelector() {
+    const emojiContainer = $('#sample-emoji-container');
+    for (let i = 0; i < emojis.length; i++) {
+        const button = $('<button>')
+        if (i === 0) {
+            button.addClass('active');
+        }
+        button.addClass('btn');
+        button.addClass('btn-default');
+        button.addClass('col-xs-2');
+        button.addClass('col-sm-1');
+        button.text(emojis[i]);
+        button.click(onEmojiSelect);
+        emojiContainer.append(button);
+    }
+}
+
+function onEmojiSelect(event) {
+    const target = $(event.target);
+    if (target.text() === '+') {
+        onOpenCustomEmoji();
+    } else {
+        selectedEmoji = target.text();        
+        submitPhrase();
+        const currentActive = $('.active');
+        currentActive.removeClass('active');
+        target.addClass('active');
+    }
+}
+
+function onOpenCustomEmoji() {
+    $('#sample-emoji-container').addClass('invisible');
+    $('#custom-emoji-container').removeClass('invisible');
+    const emojiInput = $('#custom-emoji');
+    emojiInput.val('');
+    emojiInput.focus();
+}
+
+function onCloseCustomEmoji() {
+    $('#sample-emoji-container').removeClass('invisible');
+    $('#custom-emoji-container').addClass('invisible');
+    const emojiInput = $('#custom-emoji');
+    emojiInput.val('');
+}
+
+$(function () {
+    fillEmojiSelector();
+    handleUrlParams();
+
+    const phraseInput = $('#phrase');
+    phraseInput.keyup(onKeyUp);
+    phraseInput.keydown(onKeyDown);
+
+    const emojiInput = $('#custom-emoji');
+    emojiInput.keyup(onKeyUp);
+    emojiInput.keydown(onKeyDown);
+
+    $('#custom-emoji-close').click(onCloseCustomEmoji);
 });
